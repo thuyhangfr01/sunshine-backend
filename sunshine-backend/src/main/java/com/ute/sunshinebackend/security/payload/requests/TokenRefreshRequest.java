7c0b19453b46d1e4858d8c4c166063431852629e
thuyhang <thuyhangfr01@gmail.com>
2023-04-25 20:03:01 +0700

    refresh token with jwt

---
+++ sunshine-backend/src/main/java/com/ute/sunshinebackend/security/payload/requests/TokenRefreshRequest.java
@@ -0,0 +1,19 @@
+package com.ute.sunshinebackend.security.payload.requests;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class TokenRefreshRequest {
+    @NotBlank
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
